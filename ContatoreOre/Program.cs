0ffb06fca30ba520458d1b9ef7e87099504cdbba
silviu48 <silviu48vatamanelu@gmail.com>
2021-09-22 17:08:16 +0200

    Add project files.

---
+++ ContatoreOre/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContatoreOre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
